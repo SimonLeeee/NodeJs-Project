cd16801333cb013f6c2fcaa0b6ada7da18cdfc68
simon__lee <simon__lee@outlook.com>
2016-08-02 23:25:48 +0800

    完成登陆与注册

--- routes/index.js
+++ routes/index.js
@@ -2,18 +2,43 @@ var express = require('express');
 var router = express.Router();
 var user = require("../service/sql/api/user.js");
 
-/* GET home page. */
+//登陆页面
 router.get('/', function(req, res, next) {
   res.render('login');
 });
 
+//注册页面
 router.get("/register", function(req, res, next){
 	res.render("register");
 });
 
+//登陆请求
+router.post("/api/user/login", function(req, res, next){
+	user.checkAccount(req.body.account, req.body.password, function(msg, mysqlCon){
+		//返回客户端msg
+		if(msg === "success"){
+			res.render("index");
+		}else{
+			res.render("./public-model/error", { message : msg, error:{status:"error"} });
+		}
+		
+		//释放mysql连接
+		mysqlCon.release();
+	})
+});
+
+//注册请求
 router.post("/api/user/register", function(req, res, next){
-	user.add(req.body.account, req.body.password, req.body.repeat, function(msg){
-		res.send(msg);
+	user.add(req.body.account, req.body.password, req.body.repeat, function(msg, mysqlCon){
+		//返回客户端msg
+		if(msg === "success"){
+			res.send(msg);
+		}else{
+			res.render("./public-model/error", { message : msg, error:{status:"error"} });
+		}
+		
+		//释放mysql连接
+		mysqlCon.release();
 	});
 });
 
--- service/sql/api/user.js
+++ service/sql/api/user.js
@@ -1,7 +1,9 @@
 "use strict";
 
-var pool = require("../sql-root");
+var pool = require("../mysql-root");
 var check = require("../validate");
+var md5 = require("crypto");
+var md5_Key = "!@#$%^&*()_++_)(*&^%$#@!Simon";
 
 var user = {
 
@@ -12,44 +14,62 @@ var user = {
 				if(password === repeat){
 					if(check.isAccount(account)){
 						if(check.isPassword(password)){
-							con.query("INSERT INTO user(account,password) VALUES ('"+account+"','"+password+"');",function(err){
+							con.query("INSERT INTO user(account,password,md5_key) VALUES ('"+account+"','"+password+"','"+md5.createHmac("md5",md5_Key).update(account+password).digest('hex')+"');",function(err){
 								if(!err){
-									callbak("插入用户表成功");
-									con.release();
+									callbak("success", con);
 									return;
 								}else{
-									if(errno===1062){
-										callbak("该账号已被注册");
-										con.release();
+									if(err.errno===1062){
+										callbak("该账号已被注册", con);
 										return;
 									}else{
-										callbak(err);
-										con.release();
+										callbak(err, con);
 										return;
 									}
 								}
 							});
 						}else{
-							callbak("密码格式不正确");
-							con.release();
+							callbak("密码格式不正确", con);
 							return;
 						}
 					}else{
-						callbak("账号格式不正确");
-						con.release();
+						callbak("账号格式不正确", con);
 						return;
 					}
 				}else{
-					callbak("两次输入密码不同");
-					con.release();
+					callbak("两次输入密码不同", con);
 					return;
 				}
 			}else{
 				console.log(err);
 				console.log("root connect done.");
+				con.release();
 			}
-			con.release();
 		});
+	},
+
+	checkAccount : function(account, password, callbak){
+		var key = md5.createHmac("md5",md5_Key).update(account+password).digest("hex")
+		pool.getConnection(function(err, con){
+			if(!err){
+				console.log("root connect suceessful.");
+			}else{
+				callbak("sql connect done", con);
+				return;
+			}
+
+			con.query("SELECT * FROM user where md5_key='"+key+"'", function(err, msg){
+				if(!err){
+					if(msg.length){
+						callbak("success", con);
+					}else{
+						callbak("账号或密码错误", con)
+					}
+				}else{
+					callbak(err, con);
+				}
+			});
+		})
 	}
 }
 
